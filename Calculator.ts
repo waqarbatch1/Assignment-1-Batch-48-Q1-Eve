9496631f424944d44048bd980a775023cac09341
Waqar Iqbal <139140346+waqarbatch1@users.noreply.github.com>
2023-07-16 20:44:10 +0500

    Add files via upload

---
+++ Calculator.ts
@@ -0,0 +1,22 @@
+//Assignment 2, Dated: Juyl 15th, 2023, Saturday. (Evening)
+
+let num1 = 3, num2 = 5;
+var Sum = num1 + num2;
+var Difference = num1 - num2;
+var Prodcut = num1*num2;
+var Division = num1/num2;
+var Exponent_num1 = num1**num1;
+var Exponent_num2 = num2**num2;
+var Modulus = num2%num1 ;
+var Sqaure_Root_num1 =  num1**0.5;
+var Sqaure_Root_num2 = num2**0.5 ;
+
+console.log("Sum of number 1 & number 2 is = " + Sum, "\n");
+console.log("Difference of number 1 & number 2 is = " + Difference, "\n");
+console.log("Product of two numbers is = " + Prodcut, "\n");
+console.log("Division of two numbers is = " + Division, "\n");
+console.log("Exponential of number 1 with respect to number 2 is = " + Exponent_num1, "\n");
+console.log("Exponential of number 2 with respect to number 1 is = " + Exponent_num2, "\n");
+console.log("Square root of number 1 is = " + Sqaure_Root_num1, "\n");
+console.log("Square root of number 2 is = " + Sqaure_Root_num2, "\n");
+console.log("Modulus of number 2 with respect to number 1 = " +Modulus);
